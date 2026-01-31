58cfd37c34f7792e35c58d89e6dd6180eaf7b60a
Ovi <133152464+ovishkh@users.noreply.github.com>
2026-02-01 00:36:11 +0600

    Fix configuration file errors and documentation paths
    - Corrected vendor/andyos/config/privacy.mk with intended AndyOS properties
    - Updated vendor/andyos/config/permissions/privapp-permissions-andyos.xml with full app permission list
    - Moved vendor documentation to vendor/andyos/README.md
    - Cleaned up temp files

--- vendor/andyos/config/privacy.mk
+++ vendor/andyos/config/privacy.mk
@@ -1,125 +1,45 @@
 # AndyOS Privacy Configuration
-# This makefile defines privacy-focused defaults for all AndyOS builds
 
-PRODUCT_NAME := AndyOS
-PRODUCT_BRAND := AndyOS
-PRODUCT_MANUFACTURER := AndyOS Project
-
-# Version info
-PRODUCT_VERSION_MAJOR := 1
-PRODUCT_VERSION_MINOR := 0
-PRODUCT_VERSION_CODENAME := Liberation
-
-# Privacy-respecting DNS (Quad9)
+# Privacy-focused system properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    net.dns1=9.9.9.9 \
-    net.dns2=149.112.112.112 \
-    net.dns3=2620:fe::fe \
-    net.dns4=2620:fe::9
-
-# Replace Google connectivity checks
-PRODUCT_PROPERTY_OVERRIDES += \
-    captive_portal_https_url=https://captiveportal.andyos.org/generate_204 \
-    captive_portal_http_url=http://captiveportal.andyos.org/generate_204 \
-    captive_portal_fallback_url=http://captiveportal.andyos.org/fallback \
-    captive_portal_other_fallback_urls=http://captiveportal.andyos.org/other_fallback
+    ro.url.legal=https://andyos.org/legal \
+    ro.url.legal.android_privacy=https://andyos.org/privacy \
+    ro.setupwizard.mode=OPTIONAL \
+    setupwizard.feature.baseline_setupwizard_enabled=true
 
 # Disable Google services
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.clientidbase= \
-    ro.com.google.clientidbase.ms= \
-    ro.com.google.clientidbase.am= \
-    ro.com.google.clientidbase.gmm= \
-    ro.com.google.clientidbase.yt=
-
-# Privacy defaults
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.setupwizard.mode=DISABLED \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
-    ro.opa.eligible_device=false \
-    ro.com.android.dataroaming=false \
-    ro.config.nocheckin=yes
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
 
-# Disable telemetry and crash reporting
+# DNS Configuration
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.error.receiver.system.apps= \
-    ro.atrace.core.services= \
-    persist.traced.enable=0 \
-    debug.atrace.tags.enableflags=0
+    net.dns1=9.9.9.9 \
+    net.dns2=149.112.112.112 \
+    net.rmnet0.dns1=9.9.9.9 \
+    net.rmnet0.dns2=149.112.112.112
 
-# NTP servers (privacy-focused)
+# Captive portal
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.backup.ntpServer=time.cloudflare.com
+    captive_portal_https_url=https://captiveportal.andyos.org/generate_204 \
+    captive_portal_http_url=http://captiveportal.andyos.org/generate_204
 
-# Remove Google packages
-PRODUCT_PACKAGES_REMOVE += \
-    GoogleContacts \
-    GoogleDialer \
-    GoogleCamera \
-    Maps \
-    YouTube \
-    Gmail \
-    Chrome \
-    GoogleTTS \
-    GoogleExtServices \
-    GoogleServicesFramework \
-    GmsCore \
-    PrebuiltGmsCore
+# Disable telemetry
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.nocheckin=1 \
+    ro.com.google.gmsversion=0
 
-# AndyOS system apps
+# AndyOS System Apps
 PRODUCT_PACKAGES += \
     AndyOSPrivacyGuard \
     AndyOSSetupWizard \
     AndyOSSettings \
-    AndyOSUpdater
+    AndyOSUpdater \
+    AndyOSBrowser
 
-# F-Droid integration
-PRODUCT_PACKAGES += \
-    F-Droid \
-    F-DroidPrivilegedExtension
-
-# Essential AOSP apps (privacy-audited)
-PRODUCT_PACKAGES += \
-    Dialer \
-    Contacts \
-    Messaging \
-    Camera2 \
-    Gallery2 \
-    Browser \
-    Email \
-    Calendar \
-    Clock \
-    Calculator \
-    Files
-
-# Privacy tools
-PRODUCT_PACKAGES += \
-    Orbot \
-    Tor \
-    PrivacyBrowser
-
-# Security enhancements
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.adb.secure=1 \
-    persist.sys.usb.config=none \
-    ro.secure=1 \
-    ro.debuggable=0
-
-# SELinux enforcing
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
-
-# Disable OTA to Google servers
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.ota.url=https://ota.andyos.org/api/v1/updates
-
-# Branding
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.andyos.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.andyos.codename=$(PRODUCT_VERSION_CODENAME) \
-    ro.andyos.build.date=$(shell date +%Y%m%d)
-
-# Copy privacy configuration files
-PRODUCT_COPY_FILES += \
-    vendor/andyos/config/permissions/privapp-permissions-andyos.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-andyos.xml \
-    vendor/andyos/config/sysconfig/andyos-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/andyos-sysconfig.xml
+# Remove Google connectivity check
+PRODUCT_PACKAGES_REMOVE += \
+    GoogleConnectivityCheck
